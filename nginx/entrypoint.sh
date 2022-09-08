a9f4487315050a22c7cabb143d80ea4503515321
Darkhan <darkcanvas113v@gmail.com>
2022-09-08 15:17:20 +0900

    Добавил скрипт для перезагрузки nginx при изменении сертификатов.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
+# If no certificates are obtained, create dummy ones to start the nginx
 if [ ! -d "${KEYS_FOLDER}" ]
 then mkdir -p "${KEYS_FOLDER}" &&
 echo "SSL certificates are not present creating dummy ones" &&
 openssl req -batch -newkey rsa:2048 -new -nodes -x509 -days 2 -keyout "${KEYS_FOLDER}"privkey.pem -out "${KEYS_FOLDER}"fullchain.pem
 fi
 
-export NGINX_ENTRYPOINT_QUIET_LOGS=0
+./watch-for-certificate-updates.sh &
 
-echo "Running entrypoint script from image"
+# Running entrypoint script from base image
 ./docker-entrypoint.sh nginx -g "daemon off;"
